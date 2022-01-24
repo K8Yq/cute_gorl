30f661fda0bd451636380a8452b64b14272f0a4d
Katy <katy4w@gmail.com>
2022-01-25 00:27:17 +0100

    fixed a few functions in vector2 and 3 and added doc comments to most functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 mod local_tests;
 pub mod vector2;
 pub mod vector3;
-mod quadtree;
 
 pub mod cute {
     pub fn cuter()->i32{
@@ -9,7 +8,6 @@ pub mod cute {
         4
     }
 }
-
 mod math {
     pub const EPSILON: f64 = 1e-8;
     pub fn cosq(sin_a: f64) -> f64 { (1.0 - sin_a*sin_a).sqrt() }
